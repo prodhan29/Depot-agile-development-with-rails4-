5f2237b9730d554bb5af66322c1b303aaff2eec4
prodhan29 <02nahid02@gmail.com>
2016-10-17 17:13:09 +0600

    Ajax call, User creation, sending mail on user creation is done!

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -29,12 +29,15 @@ class LineItemsController < ApplicationController
   def create
     product = Product.find(params[:product_id])
     @line_item = @cart.add_product(product.id)
-
+    puts @line_item.inspect
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to store_index_url }
         format.json { render :show, status: :created, location: @line_item }
+        format.js {@current_item = @line_item}
       else
+        puts "*********line item not saved********"
+        puts @line_item.errors.full_messages
         format.html { render :new }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
       end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161014055713) do
+ActiveRecord::Schema.define(version: 20161017072810) do
 
   create_table "carts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.datetime "created_at", null: false
@@ -23,8 +23,19 @@ ActiveRecord::Schema.define(version: 20161014055713) do
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
     t.integer  "quantity",   default: 1
+    t.integer  "order_id"
     t.index ["cart_id"], name: "index_line_items_on_cart_id", using: :btree
+    t.index ["order_id"], name: "index_line_items_on_order_id", using: :btree
     t.index ["product_id"], name: "index_line_items_on_product_id", using: :btree
+  end
+
+  create_table "orders", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.text     "address",    limit: 65535
+    t.string   "email"
+    t.string   "pay_type"
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
   create_table "products", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -36,6 +47,15 @@ ActiveRecord::Schema.define(version: 20161014055713) do
     t.datetime "updated_at",                               null: false
   end
 
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "login"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   add_foreign_key "line_items", "carts"
+  add_foreign_key "line_items", "orders"
   add_foreign_key "line_items", "products"
 end
