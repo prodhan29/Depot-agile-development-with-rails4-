c87fb708184f382d90c1f518101123ec59aebf47
prodhan29 <02nahid02@gmail.com>
2016-10-18 17:00:59 +0600

    errors in order page is solved

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
 
   include CurrentCart
-  skip_before_action :authorize, only: [:new, :create]
+
   before_action :set_cart
   before_action :set_order, only: [:show, :edit, :update, :destroy]
 
